a97697e424ef128e47153bfc79b71bc793c290ef
GoldLion1011 <yogirose79@gmail.com>
2022-05-20 09:38:31 -0700

    printing w puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,14 @@
 #include<stdio.h>
+
+/**
+ * main - Void
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-puts("'"'Programming is like building a multilingual puzzle");
-getchar();
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	getchar();
+	return (0);
 }
