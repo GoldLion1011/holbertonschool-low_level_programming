a088aa26b91e17ba7566870fc0c9488c41cf5d14
GoldLion1011 <yogirose79@gmail.com>
2022-05-20 09:48:40 -0700

    printing w printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+* main - Void
+* Print - with proper grammar...
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	getchar();
+	return (0);
+}
