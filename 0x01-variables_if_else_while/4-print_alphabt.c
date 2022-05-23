f4fef974beeb340a23b6dced1985a6a9e419ffca
GoldLion1011 <yogirose79@gmail.com>
2022-05-23 12:04:31 -0700

    alphabet soup

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  *
+  * using && to exclude chars
+  *
+  * Return - Always 0 (Success)
+  */
+
+int main(void)
+{
+	int c;
+
+	c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
+			c++;
+		}
+	}
+	putchar ('\n');
+	return (0);
+}
