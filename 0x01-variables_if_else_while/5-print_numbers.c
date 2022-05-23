85b63ad9f73482d21d61da450b38787e0834933a
GoldLion1011 <yogirose79@gmail.com>
2022-05-23 13:04:16 -0700

    base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  *
+  * printf all base nums
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	printf("0123456789");
+	return (0);
+}
