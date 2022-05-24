22cada9eb51e8b098088ca81d56a3749d7a174ef
GoldLion1011 <yogirose79@gmail.com>
2022-05-24 08:11:58 -0700

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * putchar alphabet in reverse
+  *
+  * Return: Always 0 (Success)
+  */
+
+int main(void)
+{
+	char z = '0';
+
+	char y = 'a';
+
+/*	while (z <= '9');
+	{
+		putchar(z);
+		z++;
+	}
+	while (y <= 'f');
+	{
+		putchar(y);
+		y++;
+	}	*/
+	for (z = '0'; z <= '9'; z++)
+		putchar(z);
+	for (y = 'a'; y <= 'f'; y++)
+		putchar(y);	
+	putchar('\n');
+	return (0);
+}
