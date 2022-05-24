6f1bfd848c7a3d768448b57e5f3bf7acaa0d6b7d
GoldLion1011 <yogirose79@gmail.com>
2022-05-24 13:50:41 -0700

    exercise 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+  * int_isalpha - return 1 if lowercase, 0 if not
+  * Return: 1 or 0 (Success)
+  */
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	return (0);
+}
