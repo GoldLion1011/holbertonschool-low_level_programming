13227502484ec1c7f48701427807174ba11b83ed
GoldLion1011 <yogirose79@gmail.com>
2022-05-24 06:58:33 -0700

    numberZ

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * putchar all base nums
+  *
+  * Return: Always 0 (Success)
+  */
+
+int main(void)
+{
+	int n;
+
+	n = '0';
+
+	while (n <= '9');
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
