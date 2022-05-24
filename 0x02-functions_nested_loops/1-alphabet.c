904ae43a4d4a65592e800063e588bbd6380bbccd
GoldLion1011 <yogirose79@gmail.com>
2022-05-24 11:22:43 -0700

    exercise 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <main.h>
+/**
+  * main - Check the code
+  *
+  * Return: Always 0 (Success)
+  */
+
+void print_alphabet(void);
+{
+	int c;
+
+	for (c = 'a'; c <= 'z'; c++);
+	{
+		_putchar(c)
+	}
+	return (0);
+}
