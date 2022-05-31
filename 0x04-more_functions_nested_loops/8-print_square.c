3ec0a4dbbafc1b792b9dcf455c9ffa04673671ae
GoldLion1011 <yogirose79@gmail.com>
2022-05-31 12:33:18 -0700

    exercise 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,11 +9,17 @@ void print_square(int size)
 {
 	int x;
 
+	int y;
+
 	for (x = 0; x < size; x++)
 	{
-		_putchar('#');
+		for (y = 0; y < size; y++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
 	}
-	if (size <= '0')
+	if (size <= 0)
 	{
 		_putchar('\n');
 	}
