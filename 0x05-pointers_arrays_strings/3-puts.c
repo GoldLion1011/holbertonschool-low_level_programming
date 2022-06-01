45ab16584bac4fb9d1dacbf84fd3c674316c0e2f
GoldLion1011 <yogirose79@gmail.com>
2022-06-01 12:38:09 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * _puts - prints a string followed by a new line
+  * @str: string to be printed to stdout
+  * Return: string statement
+  */
+
+void _puts(char *str)
+{
+	int j = 0;
+
+	while (str[j] != '\n' && str[j] != '\0')
+	{
+		_putchar(str[j]);
+		j++;
+	}
+}
