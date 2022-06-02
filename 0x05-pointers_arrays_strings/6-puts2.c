f37cdd82eb1455cc1430967a0a47479eed7772f0
GoldLion1011 <yogirose79@gmail.com>
2022-06-02 09:06:11 -0700

    betty

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
-  *
-  *
-  *
+  * puts2 - prints every other character of a string
+  * @str: string to be referenced for output
+  * Return: void (Success)
   */
 
 void puts2(char *str)
