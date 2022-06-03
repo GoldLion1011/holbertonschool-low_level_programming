3ff06d4e5cfb9b35ed6dab0ca9bb992ee2a83ee0
GoldLion1011 <yogirose79@gmail.com>
2022-06-03 07:39:54 -0700

    corrected 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -15,9 +15,9 @@ void puts2(char *str)
 	for (len = 0; str[len] != '\0'; len++)
 	{
 	}
-	for (i = 0; i < len; i += 20)
+	for (i = 0; i < len; i += 2)
 	{
-		_putchar(str[len]);
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 }
