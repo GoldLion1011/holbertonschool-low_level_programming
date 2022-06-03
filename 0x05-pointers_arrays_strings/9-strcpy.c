5a8c2b719d41dce4cf8a9f9f828c6970ffc014a3
GoldLion1011 <yogirose79@gmail.com>
2022-06-03 11:27:33 -0700

    task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -2,12 +2,21 @@
 
 /**
   * *_strcpy - copies a pointed to string
-  *
-  *
+  * @src: pointer to array to be copied
+  * @dest: destination that src is copied to
   * Return: the pointer to dest (success)
   */
 
 char *_strcpy(char *dest, char *src)
 {
-	
-	
+	char *copier = src;
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = *copier;	
+	return (dest);
+}	
