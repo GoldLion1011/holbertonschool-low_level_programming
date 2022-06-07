953f73f9363432dd0e9271214cc1c94a14e80f59
GoldLion1011 <yogirose79@gmail.com>
2022-06-07 13:35:22 -0700

    changed to while loop

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,12 +9,15 @@
 
 char *_strchr(char *s, char c)
 {
-	if (*s == c)
+	while (*s)
 	{
+		if (*s != c)
+			s++;
+		else
+			return (s);
+	}
+	if (c == '\0')
 		return (s);
-	}
-	while (*s++)
-	{
-		return ('\0');
-	}
+
+	return ('\0');
 }
