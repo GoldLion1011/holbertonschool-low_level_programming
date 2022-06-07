00c734a3fff70c7b6373252c72b7fed4f3e38609
GoldLion1011 <yogirose79@gmail.com>
2022-06-07 08:43:05 -0700

    task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  *
+  *
+  *
+  *
+  */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	char *src_char = (char *)src;
+
+	char *dest_char = (char *)dest;
+
+	for (i = 0; i < n; i++)
+		dest_char[i] = src_char[i];
+	
+	return (dest);
+}
