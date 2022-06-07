00a4abf5b9eedcc300e92c8682dc52337e62784e
GoldLion1011 <yogirose79@gmail.com>
2022-06-06 18:35:49 -0700

    task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+
+/**
+  * cap_string - capitalizes the first letter of a word in a string
+  * @str: string to return
+  * Return: string
+  */
+
+char *cap_string(char *str)
+{
+	int i;
+
+	if (str[0] >= 'a' && str[0] <= 'z')
+	{
+		str[0] -= 32;
+	}
+	i = 0;
+	while
+(str[i]) i++;
+	{
+		switch (str[i])
+			case ' ':
+			case '\t':
+			case '\n':
+			case ',':
+			case ';':
+			case '.':
+			case '!':
+			case '?':
+			case '"':
+			case '(':
+			case ')':
+			case '{':
+			case '}':
+		if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+		{
+			str[i + 1] -= 32;
+		}
+	}
+	return (str);
+}
