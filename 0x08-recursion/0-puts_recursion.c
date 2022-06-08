dcd741809a45dc18de51d9eee6af878ef2bcef3f
GoldLion1011 <yogirose79@gmail.com>
2022-06-08 11:24:03 -0700

    betty issue

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-  * _print_rev_recursion - prints a string followed by a new line
+  * _puts_recursion - prints a string followed by a new line
   * @s: pointer to a string to be printed
   * Return: nothing
   */
