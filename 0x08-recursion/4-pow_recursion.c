0e5e57feeabfea7658f0e7664ae3895507867eb3
GoldLion1011 <yogirose79@gmail.com>
2022-06-08 11:40:11 -0700

    total rewrite

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -9,15 +9,13 @@
 
 int _pow_recursion(int x, int y)
 {
-	int power = 1, i;
+	int power = x;
 
-	for (i = 1; i <= y; ++i)
-	{
-		power = power * x;
-	}
-	if (i == 0)
-	{
+	if (y < 0)
 		return (-1);
-	}
+	else if (y == 0)
+		return (1);
+	power *= _pow_recursion(x, y - 1);
+
 	return (power);
 }
