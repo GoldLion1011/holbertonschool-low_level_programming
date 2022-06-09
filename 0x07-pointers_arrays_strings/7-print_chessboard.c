d98ddcaaaa58ae2321a3c1c1f515cbb63cb28f59
GoldLion1011 <yogirose79@gmail.com>
2022-06-09 15:27:38 -0700

    task 7 redo

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -10,16 +10,11 @@ void print_chessboard(char (*a)[8])
 {
 	int b, w;
 
-	for (b = 0; b < 8; b++)
+	for (b = 0; a[b][7]; b++)
 	{
 		for (w = 0; w < 8; w++)
-		{
-			if (w == 7)
-			{
 				_putchar(a[b][w]);
-				_putchar('/n');
-			}
-			else
-				_putchar(a[b][w]);
-		}
+				
+		_putchar('/n');
+	}
 }
