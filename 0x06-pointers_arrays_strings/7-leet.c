1dc7eed956a66c2ac3d064a5c08e49866315d31f
GoldLion1011 <yogirose79@gmail.com>
2022-06-09 07:47:46 -0700

    task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,4 +10,24 @@
 
 char *leet(char *s)
 {
+	char *letters = "aAeEoOtTlL";
+
+	char *numbers = "4433007711";
+
+	int i, j;
+
+	i = 0;
+	while (s[i] != '\0')
+
+	j = 0;
+	while (letters[i] != '\0')
+	{
+		if (s[i] == letters[j])
+		{
+			s[i] = numbers[j];
+		}
+		j++;
+	}
+	i++;
+	return (s);
 }
