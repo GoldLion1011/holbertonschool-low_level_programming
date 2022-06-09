0b52f1cd7388199a062c73fea3eab39bc2fcfaab
GoldLion1011 <yogirose79@gmail.com>
2022-06-09 16:02:51 -0700

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -4,9 +4,11 @@ int find_sqrt(int num, int root);
 int sqrt_recursion(int n);
 
 /**
-  *
-  *
-  *
+  * find_sqrt - finds natural square root of a number
+  * @num: number to find sq root of
+  * @root: root to test
+  * Return: square root if number has one
+  *         -1 if number doesn't have one
   */
 
 int find_sqrt(int num, int root)
@@ -17,15 +19,14 @@ int find_sqrt(int num, int root)
 	if (root == num / 2)
 		return (-1);
 
-	return(find_sqrt(num, root + 1));
+	return (find_sqrt(num, root + 1));
 }
 
 /**
-  *
-  *
-  *
-  *
-  *
+  * _sqrt_recursion - returns natural squar root of a number
+  * @n: number to return sq root of
+  * Return: natural sq root of n if true
+  *         -1 if false
   */
 
 int _sqrt_recursion(int n)
@@ -40,4 +41,3 @@ int _sqrt_recursion(int n)
 
 	return (find_sqrt(n, root));
 }
- 
