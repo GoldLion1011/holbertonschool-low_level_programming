6aa11fd3d6ed5927bb36fb898a57410490dbf885
GoldLion1011 <yogirose79@gmail.com>
2022-06-21 12:20:52 -0700

    task 1 revised big time

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,51 +1,49 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
-  *
-  *
-  *
-  *
+  * string_nconcat - concatenates two strings
+  * @s1: first string
+  * @s2: second string
+  * @n: numbers of chars to concatenate
+  * Return: pointer to new string
   */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *array;
-	unsigned int h;
-	unsigned int i;
-	unsigned int c;
-	unsigned int d;
+	char *a = NULL;
+	unsigned int h, i, j, k;
 
 	if (s1 == NULL)
-		s1 = " ";
-
+		s1 = "";
 	if (s2 == NULL)
-		s2 = " ";
-
-	for (c = 0; *(s1 + c); c++)
-		;
-
-	for (d = 0; *(s2 + d); d++)
-		;
-
-	d++;
-
-	if (n > d)
-		s = malloc((c + d) * sizeof(*s));
-
-	else
-		s = malloc((c + n + 1) * sizeof(*s));
-
-	if (s == NULL)
-		return (NULL);
-
-	for (h = 0; h < c; h++)
-		*(s + h) = *(s1 + h);
-
-	for (i = 0; i < d; && i < n; h++; i++)
-		*(s + h) = *(s2 + i);
-
-	*(s + h) = '\0';
-
-	return (s);
+		s2 = "";
+	h = 0;
+	if (s1)
+	{
+		for (h = 0; s1[h] != '\0'; h++)
+			;
+	}
+	i = 0;
+	if (s2)
+	{
+		for (i = 0; s2[i] != '\0'; i++)
+			;
+	}
+	if (n < i)
+	{
+		i = n;
+	}
+	a = malloc(h + i + 1);
+	if (a)
+	{
+		for (j = 0; (j < h) && s1; j++)
+		{
+			a[j] = s1[j];
+		}
+		for (k = 0; (k < i) && s2; k++)
+		{
+			a[j + 1] = s2[k];
+		}
+		a[j + 1] = '\0';
+	}
+	return (a);
 }
