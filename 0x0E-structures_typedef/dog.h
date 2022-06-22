017cf2e4096676eb7db53fcf547294d71ca8513c
GoldLion1011 <yogirose79@gmail.com>
2022-06-22 09:33:04 -0700

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,25 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+  * dog - struct with elements name, age, and owner
+  * @name: doggo's name
+  * @age: doggo's age
+  * @owner: hoomans' name
+  *
+  * Description: longer description
+  */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+//void init_dog(struct dog *d, char *name, float age, char *owner);
+//void print_dog(struct dog *d);
+//dog_t *new_dog(char *name, float age, char *owner);
+//void free_dog(dog_t *d);
+
+#endif
