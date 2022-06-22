62b212aea1bb7386498dacb59e8137f4387723ab
GoldLion1011 <yogirose79@gmail.com>
2022-06-22 10:39:42 -0700

    changed header def

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef HEADER_FILE
+#define HEADER_FILE dog.h
 
 /**
   * dog - struct with elements name, age, and owner
@@ -17,9 +17,7 @@ struct dog
 	char *owner;
 };
 
-//void init_dog(struct dog *d, char *name, float age, char *owner);
-//void print_dog(struct dog *d);
-//dog_t *new_dog(char *name, float age, char *owner);
-//void free_dog(dog_t *d);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 #endif
