40dab09c99d0cf1fd11a3c18665eed042cc199b1
GoldLion1011 <yogirose79@gmail.com>
2022-06-23 10:49:07 -0700

    .h and task 1 minir tweaks

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -3,12 +3,6 @@
 
 #include <stdlib.h>
 
-/**
-  * File: function_pointers.h
-  * Description: header file containing prototypes for functions used
-  *              in project 0x10 C-Function pointers
-  */
-
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
