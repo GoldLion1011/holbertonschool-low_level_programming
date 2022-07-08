2331e1328b36a6ad8edc832cea20fe08136ed193
GoldLion1011 <yogirose79@gmail.com>
2022-07-08 10:49:31 -0700

    added _strlen func, simplified code

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -2,6 +2,21 @@
 #include <string.h>
 
 /**
+  * _strlen - gives the length of a string
+  * @s: this is the given string to count
+  * Return: length of string
+  */
+
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while
+(s[length] != '\0') length++;
+	return (length);
+}
+
+/**
   *
   *
   *
@@ -11,25 +26,22 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
-	int len;
-	char *h;
 
+	if (str == NULL || head == NULL)
+		return (NULL);
+	
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
 
-	h = strdup(str);
-	if (h == NULL)
+	new->str = strdup(str);
+	if (new->str == NULL)
 	{
 		free(new);
 		return (NULL);
 	}
-
-	for (len = 0; str[len];)
-		len++;
-
-	new->str = h;
-	new->len = len;
+	
+	new->len = _strlen(new->str);
 	new->next = *head;
 	*head = new;
 
